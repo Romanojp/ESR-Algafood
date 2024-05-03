252c1776d040d9bfa436d6ad95958c60184df778
Romanojp <jp_romana@outlook.com>
2024-05-03 16:34:51 -0300

    4.28. Refatorando a exclusão de cozinhas para usar domain services

--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -10,5 +10,5 @@ public interface CozinhaRepository {
 	List<Cozinha> listar();
 	Cozinha buscar(Long id);
 	Cozinha salvar(Cozinha cozinha);
-	void remover(Cozinha cozinha);
+	void remover(Long id);
 }
