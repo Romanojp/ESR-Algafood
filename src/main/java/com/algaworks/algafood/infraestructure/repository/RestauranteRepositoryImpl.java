341bb4f619493b3206580c320ed9f0bf594e210d
Romanojp <jp_romana@outlook.com>
2024-05-03 21:36:19 -0300

    4.29. Desafio: modelando e implementando a consulta de recursos de restaurantes

--- src/main/java/com/algaworks/algafood/infraestructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infraestructure/repository/RestauranteRepositoryImpl.java
@@ -3,14 +3,18 @@ package com.algaworks.algafood.infraestructure.repository;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 
+@Component
 public class RestauranteRepositoryImpl implements RestauranteRepository {
 	
+	@PersistenceContext
 	private EntityManager manager;
 
 	@Override
