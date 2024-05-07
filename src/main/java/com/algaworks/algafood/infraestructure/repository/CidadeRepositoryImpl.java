a00fdeeb60d5a34c90ae68281af0e5ef80d109ef
Romanojp <jp_romana@outlook.com>
2024-05-06 23:06:46 -0300

    4.32. Desafio: implementando serviços REST de cidades e estados

--- src/main/java/com/algaworks/algafood/infraestructure/repository/CidadeRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infraestructure/repository/CidadeRepositoryImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.Cidade;
@@ -34,8 +35,14 @@ public class CidadeRepositoryImpl implements CidadeRepository {
 
 	@Transactional
 	@Override
-	public void remover(Cidade cidade) {
-		cidade = buscar(cidade.getId());
+	public void remover(Long id) {
+		
+		Cidade cidade = buscar(id);
+		
+		if (cidade == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		
 		manager.remove(cidade);
 		
 	}
