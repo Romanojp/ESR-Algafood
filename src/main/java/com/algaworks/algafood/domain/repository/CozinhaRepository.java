1e848191e184b4379686aa59d74c40014e410fc4
Romanojp <jp_romana@outlook.com>
2024-06-06 18:31:04 -0300

    5.7. Usando as keywords para definir critérios de query methods

--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -10,7 +10,14 @@ import com.algaworks.algafood.domain.model.Cozinha;
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
+	//buca por nome 
 	List<Cozinha> findByNome(String nome);
+	
+	//busca por LIKE 
+	List<Cozinha> findByNomeContaining(String nome);
+	
+	//https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html (keywords de exemplos)
+	
 
 
 }
