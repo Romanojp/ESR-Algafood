527408cb86bd05195de8891afa942c85a8b203b2
Romanojp <jp_romana@outlook.com>
2024-06-23 22:31:27 -0300

    5.14. Adicionando restrições na cláusula where com Criteria API

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.model.Cozinha;
@@ -63,8 +64,8 @@ public class TesteController {
 		}
 		
 		@GetMapping("/restaurantes/por-nome-e-frete")
-		public List<Restaurante> restaurantesPorNomeFrete(String nome, 
-				BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+		public List<Restaurante> restaurantesPorNomeFrete( String nome, 
+				 BigDecimal taxaFreteInicial,  BigDecimal taxaFreteFinal) {
 			return restauranteRepository.find(nome, taxaFreteInicial, taxaFreteFinal);
 		}
 		
